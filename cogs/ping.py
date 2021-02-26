7fbf0c4c564e12d773e4012f7c19e080730ae3fc
Req-kun <69065737+Req-kun@users.noreply.github.com>
2021-02-26 15:23:12 +0900

    Add files via upload

---
+++ cogs/ping.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import asyncio
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command(name="ping")
+    async def _ping(self, ctx):
+        await ctx.reply(f'Pong! :ping_pong: `{round(self.bot.latency*1000, 3)}ms`', mention_author=False)
+        return
+
+def setup(bot):
+    return bot.add_cog(Ping(bot))
