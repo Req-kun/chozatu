68339520c9eda65ffa442d95e4a9843a661d0064
Req-kun <69065737+Req-kun@users.noreply.github.com>
2021-03-02 15:32:05 +0900

    Add files via upload

---
+++ cogs/someone.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Someone(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.content == '@someone'
+            return
+        if not message.author.guild_permissions.administrator:
+            return
+        member = random.choice(message.guild.members)
+        await message.reply(member.mention, mention_author=False)
+        return
+
+
+
+
+def setup(bot):
+    return bot.add_cog(Fugou(bot))
