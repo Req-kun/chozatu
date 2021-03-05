b8e48218f801ff8c8a01035a6a90039f25402b82
Req-kun <69065737+Req-kun@users.noreply.github.com>
2021-03-05 13:46:25 +0900

    いろいろ追加

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import asyncio
+from datetime import datetime
+
+
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,7 +11,15 @@ class Ping(commands.Cog):
 
     @commands.command(name="ping")
     async def _ping(self, ctx):
-        await ctx.reply(f'Pong! :ping_pong: `{round(self.bot.latency*1000, 3)}ms`', mention_author=False)
+        now = datetime.now()
+        msg = await ctx.send(embed=discord.Embed(title='計測中...', color=discord.Colour.dark_theme()))
+        created = msg.created_at
+
+        command_latency = (now - ctx.message.created_at).microseconds
+        message_latency = (msg.created_at - now).microseconds
+        socket_latency = self.bot.latency
+
+        await msg.edit(content=f'Pong! :ping_pong:', embed=(discord.Embed(title='計測結果', description=f'websocket: `{round(socket_latency*1000, 1)}ms`\nmessage: `{round(message_latency/1000, 1)}`\ncommand: `{round(command_latency/1000, 1)}`')).set_footer(text='この計測値はあくまでも目安であり、正しくない可能性があります'))
         return
 
 def setup(bot):
