f7d401165883a31df0f2bc39239abcfa7a05bd6e
Req-kun <69065737+Req-kun@users.noreply.github.com>
2021-03-08 14:23:40 +0900

    Add files via upload

---
+++ cogs/empty.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord_slash import cog_ext
+from discord_slash import SlashCommand
+from discord_slash import SlashContext
+
+class Empty(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_subcommand(
+        base="fun", name="empty",
+        base_description="楽しもう！", description="「空白」を送信いたします",
+        guild_ids=[733707710784340100]
+    )
+    async def _empty(self, ctx):
+        msg = await ctx.send(embeds=[discord.Embed()])
+        await msg.edit(flags=4)
+        return
+
+
+
+def setup(bot):
+    bot.add_cog(Empty(bot))
