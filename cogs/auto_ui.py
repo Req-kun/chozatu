2c2d4afac870bfce12684247cc596fd23bddc9da
Req-kun <69065737+Req-kun@users.noreply.github.com>
2021-04-17 18:36:28 +0900

    approveされた表記

--- cogs/auto_ui.py
+++ cogs/auto_ui.py
@@ -68,7 +68,7 @@ class Autoui(commands.Cog):
         
                         # 送信待機メッセージ判定３
                         if embed.author.name.endswith(f'{id})'):
-                            await msg.edit(embed=discord.Embed('approved').set_author(name=f'{msg.author.name}(ID:{msg.author.id})', icon_url=msg.author.avatar_url))
+                            await msg.edit(embed=discord.Embed(title='approved').set_author(name=f'{msg.author.name}(ID:{msg.author.id})', icon_url=msg.author.avatar_url))
                             return
             return
         
